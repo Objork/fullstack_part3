bfe453851c0be124e289dd302d916ac22649bc04
Oskar <oskar.bjorkgren@aalto.fi>
2022-07-12 18:09:53 +0300

    working on 3.11

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -5,9 +5,9 @@ const cors = require('cors')
 
 const app = express()
 
-
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :param'),)
