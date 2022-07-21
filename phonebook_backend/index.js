92ad616d7c025e94f396f4638400a39221ab1a73
Oskar <oskar.bjorkgren@aalto.fi>
2022-07-21 22:52:42 +0300

    working on part 3

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
+const { request } = require('http')
 
 
 const app = express()
@@ -74,12 +75,26 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
 
     Person.findByIdAndRemove(request.params.id)
         .then(result => {
             response.status(204).end()
         })
+        .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, respone, next) => {
+    const body = request.body
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            respone.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -117,6 +132,15 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, respone, next) => {
+    if (error.name === 'CastError') {
+        return respone.status(400).send({ error })
+    }
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
