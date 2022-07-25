927579a6cd7f867b9780e11783170a2dee960751
Oskar <oskar.bjorkgren@aalto.fi>
2022-07-25 16:28:14 +0300

    eslint

--- phonebook_backend/mongo.js
+++ phonebook_backend/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length !== 3 && process.argv.length !== 5) {
-    console.log("Something went wrong when inputting the arguments")
-    process.exit(1)
+  console.log('Something went wrong when inputting the arguments')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -13,36 +13,36 @@ const url = `mongodb+srv://objorkgr:${password}@cluster0.vd04xqz.mongodb.net/?re
 
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 
 })
 
 mongoose
-    .connect(url)
-    .then((result) => {
-        console.log('connected to Mongo')
-    })
-    .catch((err) => console.log(err))
+  .connect(url)
+  .then((result) => {
+    console.log('connected to Mongo')
+  })
+  .catch((err) => console.log(err))
 
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 5) {
 
-    const person = new Person({
-        name: person_name,
-        number: person_num
-    })
-    person.save().then(result => {
-        console.log(`Added ${person.name} number ${person_num} to phonebook`)
-        mongoose.connection.close()
-    })
+  const person = new Person({
+    name: person_name,
+    number: person_num
+  })
+  person.save().then(result => {
+    console.log(`Added ${person.name} number ${person_num} to phonebook`)
+    mongoose.connection.close()
+  })
 }
 
 else {
-    Person.find({}).then(result => {
-        console.log("phonebook:")
-        result.forEach(person => { console.log(`${person.name} ${person.number}`) })
-        mongoose.connection.close()
-    })
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => { console.log(`${person.name} ${person.number}`) })
+    mongoose.connection.close()
+  })
 }
